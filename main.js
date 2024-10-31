0c8045bf9ae3e6e1fe223807a73f85382ca903c0
yousseforabi <122828573+yousseforabi@users.noreply.github.com>
2024-10-31 10:20:04 +0100

    WRITING THE CONSOLE LOG

--- main.js
+++ main.js
@@ -9,3 +9,9 @@ function sumEvenNumbers(numbers) {
     
     return sum;
   }
+
+  //TESTING FUNCTION
+  console.log(sumEvenNumbers([1, 2, 3, 4, 5, 6])); // Expected : 12
+console.log(sumEvenNumbers([1, 3, 5, 7, 9])); // Expected : 0
+console.log(sumEvenNumbers([2, 4, 6, 8, 10])); // Expected : 30
+console.log(sumEvenNumbers([-2, 0, 2, 4, -6])); // Expected : -2
